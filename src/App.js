d7de50b56926cbb9e6d37cddfb6231eeb15bb3fc
Vedant Dandge <vedantdandgepro@gmail.com>
2022-11-17 20:19:55 +0530

    added main componenet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
-  return(
-    <>netflix clone</>
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} exact/>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/home.jsx
@@ -0,0 +1,9 @@
+import Main from "../components/Main";
+
+const Home = () => {
+  return <div>
+    <Main/>
+  </div>;
+};
+
+export default Home;
